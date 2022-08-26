a1ac6a2e4faded6f21cdb268540961a5729ff434
Ibrahim <mibrahim37612@gmail.com>
2022-08-27 00:08:33 +0500

    init commit

---
+++ src/components/News.js
@@ -0,0 +1,120 @@
+import React, { Component } from "react";
+import NewsItem from "./NewsItem";
+import Spinner from "./Spinner";
+import PropTypes from "prop-types";
+import InfiniteScroll from "react-infinite-scroll-component";
+
+export class News extends Component {
+  static defaultProps = {
+    country: "us",
+    pageSize: 8,
+    category: "general",
+  };
+  static propTypes = {
+    country: PropTypes.string,
+    pageSize: PropTypes.number,
+    category: PropTypes.string,
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      articles: [],
+      loading: false,
+      page: 1,
+      totalResults: 0,
+    };
+    document.title = `${this.capitalizeWord(this.props.category)} - NewsMonkey`;
+  }
+
+  capitalizeWord = (word) => {
+    return word.charAt(0).toUpperCase() + word.slice(1);
+  };
+
+  async updateNews() {
+    this.props.setProgress(10);
+    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&pageSize=${this.props.pageSize}&page=${this.state.page}`;
+    this.setState({ loading: true });
+    let data = await fetch(url);
+    this.props.setProgress(40);
+    let parsedData = await data.json();
+    console.log(parsedData);
+    this.props.setProgress(70);
+    this.setState({
+      articles: parsedData.articles,
+      totalResults: parsedData.totalResults,
+      loading: false,
+    });
+    this.props.setProgress(100);
+  }
+
+  async componentDidMount() {
+    this.updateNews();
+  }
+
+  handlePrevClick = async () => {
+    this.setState({ page: this.state.page - 1 });
+    this.updateNews();
+  };
+
+  handleNextClick = async () => {
+    this.setState({ page: this.state.page + 1 });
+    this.updateNews();
+  };
+
+  fetchMoreData = async () => {
+    this.setState({ page: this.state.page + 1 });
+    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${
+      this.props.category
+    }&apiKey=${this.props.apiKey}&pageSize=${this.props.pageSize}&page=${this.state.page + 1}`;
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    console.log(parsedData);
+    this.setState({
+      articles: this.state.articles.concat(parsedData.articles),
+      totalResults: parsedData.totalResults,
+      loading: false,
+    });
+  };
+
+  render() {
+    return (
+      <>
+        <h1 className="text-center mb-4">{`NewsMonkey - Top ${this.capitalizeWord(this.props.category)} Headlines`}</h1>
+        {this.state.loading && <Spinner />}
+        <InfiniteScroll
+          dataLength={this.state.articles.length}
+          next={this.fetchMoreData}
+          hasMore={this.state.articles.length < this.state.totalResults}
+          loader={<Spinner />}
+        >
+          <div className="container my-3 " style={{ margin: "auto" }}>
+            <div className="row">
+              {this.state.articles.map((element,index) => {
+                return (
+                  <div className="col-md-4" key={index}>
+                    <NewsItem   
+                      title={element.title ? element.title : "No title!"}
+                      description={element.description ? element.description : "No description!"}
+                      imageUrl={
+                        element.urlToImage
+                          ? element.urlToImage
+                          : "https://media.istockphoto.com/photos/breaking-news-world-news-with-map-backgorund-picture-id1182477852?k=20&m=1182477852&s=612x612&w=0&h=I3wdSzT_5h1y9dHq_YpZ9AqdIKg8epthr8Guva8FkPA="
+                      }
+                      newsUrl={element.url}
+                      author={element.author ? element.author : "Unknown"}
+                      date={element.publishedAt ? new Date(element.publishedAt).toGMTString() : "Unknown"}
+                      source={element.source.name ? element.source.name : "Unknown"}
+                    />
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        </InfiniteScroll>
+      </>
+    );
+  }
+}
+
+export default News;
---
+++ src/components/NewsItem.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+
+export class NewsItem extends Component {
+  render() {
+    let { title, description, imageUrl, newsUrl, author, date, source } = this.props;
+    return (
+      <div className="m-2">
+        <div className="card mx-2" style={{ maxWidth: "25rem" }}>
+          <div style={{ display: "flex", justifyContent: "flex-end", position: "absolute", right: "0" }}>
+            <span className="position-absolute top-0 badge rounded-pill bg-danger ">{source}</span>
+          </div>
+          <img src={imageUrl} className="card-img-top" alt="..." style={{ maxHeight: "12rem" }} />
+          <div className="card-body">
+            <h5 className="card-title">{title} </h5>
+            <p className="card-text">
+              {description
+                ? description.length > 88
+                  ? description.slice(0, 87).concat("...")
+                  : description
+                : "No description!"}
+            </p>
+            <p className="card-text">
+              <small className="text-muted">
+                By {author} at {date}
+              </small>
+            </p>
+            <a href={newsUrl} rel="noreferrer" target="_blank" className="btn btn-small btn-dark">
+              Read More
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default NewsItem;
